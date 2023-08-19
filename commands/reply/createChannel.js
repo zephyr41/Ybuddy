d9fc471312c9d467c9ec61ab3ca1795dca0ee1e9
Zephyr <nils.jaudon@ynov.com>
2023-08-19 15:17:01 +0200

    CreateCHannelProblem

--- commands/reply/createChannel.js
+++ commands/reply/createChannel.js
@@ -1,59 +1,78 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, response } = require('discord.js');
+const {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    SlashCommandBuilder,
+} = require('discord.js');
 
 module.exports = {
 
-	data: new SlashCommandBuilder()
-		.setName('createchannel')
-		.setDescription('va permettre de t\'envoyer vers le channel de ta filière'),
-	async execute(interaction) {
-		const confirm = new ButtonBuilder()
-			.setCustomId('confirm')
-			.setLabel('Confirm')
-			.setStyle(ButtonStyle.Success);
+        data: new SlashCommandBuilder()
+            .setName('createchannel')
+            .setDescription('va permettre de t\'envoyer vers le channel de ta filière'),
+        async execute(interaction) {
+                const confirm = new ButtonBuilder()
+                    .setCustomId('confirm')
+                    .setLabel('Confirm')
+                    .setStyle(ButtonStyle.Success);
 
-		const cancel = new ButtonBuilder()
-			.setCustomId('cancel')
-			.setLabel('Cancel')
-			.setStyle(ButtonStyle.Secondary);
+                const cancel = new ButtonBuilder()
+                    .setCustomId('cancel')
+                    .setLabel('Cancel')
+                    .setStyle(ButtonStyle.Secondary);
 
-		const row = new ActionRowBuilder()
-			.addComponents(confirm, cancel);
+                const row = new ActionRowBuilder()
+                    .addComponents(confirm, cancel);
 
-		await interaction.reply({
-			content: `Je vais crée un channel ${interaction.user.username}, pour pouvoir te rediriger vers le bon chnnel.`,
-			components: [row],
-		});
-		const collectorFilter = i => i.user.username === interaction.user.id && i.customId === 'confirm';
+                await interaction.reply({
+                    content: `Je vais crée un channel ${interaction.user.username}, pour pouvoir te rediriger vers le bon chnnel.`,
+                    components: [row],
+                });
 
-		try {
-			const confirmation = await response.awaitMessageComponent({ filter: collectorFilter });
+                const filter = i => i.customId === 'confirm' || i.customId === 'cancel';
+                const collector = interaction.channel.createMessageComponentCollector({
+                    filter,
+                    time: 15000
+                });
 
-			if (confirmation.customId === 'confirm') {
-				await confirmation.update({ content: 'Action confirmed', components: [] });
-				console.log('Action confirmed');
-			}
-			else if (confirmation.customId === 'cancel') {
-				console.log('Action cancelled');
-			}
-		}
-		catch (e) {
-			await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
-		}
+                collector.on('collect', async i => {
+                            if (i.customId === 'confirm') {
+                                await i.update({
+                                    content: 'Confirmed!',
+                                    components: []
+                                });
+                                Guild.channels.create({
+                                        name: (' CONNEXION DISCORD POUR ${interaction.user.username}'),
+                                        type: ChannelType.GuildText,
+                                        permissionOverwrites: [{
+                                            id: interaction.guild.id,
+                                            deny: [PermissionsBitField.Flags.ViewChannel],
+                                        }, ],
+                                    }
 
-	},
+                                    else if (i.customId === 'cancel') {
+                                        await i.update({
+                                            content: 'Cancelled!',
+                                            components: []
+                                        });
+                                    }
+                                },
+                            );
 
-};
-// guild.channels.create({
-// 	name: 'new-channel',
-// 	type: ChannelType.GuildText,
-// 	permissionOverwrites: [
-// 		{
-// 			id: interaction.guild.id,
-// 			deny: [PermissionsBitField.Flags.ViewChannel],
-// 		},
-// 		{
-// 			id: interaction.user.id,
-// 			allow: [PermissionsBitField.Flags.ViewChannel],
-// 		},
-// 	],
-// });
+                        },
+
+                };
+                // guild.channels.create({
+                // 	name: 'new-channel',
+                // 	type: ChannelType.GuildText,
+                // 	permissionOverwrites: [
+                // 		{
+                // 			id: interaction.guild.id,
+                // 			deny: [PermissionsBitField.Flags.ViewChannel],
+                // 		},
+                // 		{
+                // 			id: interaction.user.id,
+                // 			allow: [PermissionsBitField.Flags.ViewChannel],
+                // 		},
+                // 	],
+                // });
