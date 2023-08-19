b8bec8f1f5edc74702c6cf2de7a7ea2f9795ccc8
Zephyr <nils.jaudon@ynov.com>
2023-08-19 15:04:45 +0200

    AddButton

--- commands/reply/createChannel.js
+++ commands/reply/createChannel.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, response } = require('discord.js');
 
 module.exports = {
 
@@ -6,18 +6,54 @@ module.exports = {
 		.setName('createchannel')
 		.setDescription('va permettre de t\'envoyer vers le channel de ta filière'),
 	async execute(interaction) {
-
 		const confirm = new ButtonBuilder()
 			.setCustomId('confirm')
 			.setLabel('Confirm')
 			.setStyle(ButtonStyle.Success);
 
+		const cancel = new ButtonBuilder()
+			.setCustomId('cancel')
+			.setLabel('Cancel')
+			.setStyle(ButtonStyle.Secondary);
+
 		const row = new ActionRowBuilder()
-			.addComponents(confirm);
+			.addComponents(confirm, cancel);
 
 		await interaction.reply({
 			content: `Je vais crée un channel ${interaction.user.username}, pour pouvoir te rediriger vers le bon chnnel.`,
 			components: [row],
 		});
+		const collectorFilter = i => i.user.username === interaction.user.id && i.customId === 'confirm';
+
+		try {
+			const confirmation = await response.awaitMessageComponent({ filter: collectorFilter });
+
+			if (confirmation.customId === 'confirm') {
+				await confirmation.update({ content: 'Action confirmed', components: [] });
+				console.log('Action confirmed');
+			}
+			else if (confirmation.customId === 'cancel') {
+				console.log('Action cancelled');
+			}
+		}
+		catch (e) {
+			await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+		}
+
 	},
+
 };
+// guild.channels.create({
+// 	name: 'new-channel',
+// 	type: ChannelType.GuildText,
+// 	permissionOverwrites: [
+// 		{
+// 			id: interaction.guild.id,
+// 			deny: [PermissionsBitField.Flags.ViewChannel],
+// 		},
+// 		{
+// 			id: interaction.user.id,
+// 			allow: [PermissionsBitField.Flags.ViewChannel],
+// 		},
+// 	],
+// });
