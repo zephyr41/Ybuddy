ee2d50d0f5ecd5fa4bc71b844a2906b1f0191180
Zephyr <nils.jaudon@ynov.com>
2023-09-17 02:32:26 +0200

    SelectMenu&ButtonInteraction

--- commands/reply/joinclass.js
+++ commands/reply/joinclass.js
@@ -1,13 +1,17 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, ChannelType, PermissionsBitField, Events, ModalBuilder, TextInputBuilder, TextInputStyle, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
+
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, ChannelType, PermissionsBitField, Events, ModalBuilder, TextInputBuilder, TextInputStyle, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ComponentType } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
+        // donne un nom à la commande
         .setName('joinclass')
-        .setDescription('va permettre de t\'envoyer vers le channel de ta filière buddy ;)'),
+        .setDescription('Va permettre de t\'envoyer vers le channel de ta filière buddy ;)'),
+
     async execute(interaction) {
+        // fait la liste des menu
         const select = new StringSelectMenuBuilder()
             .setCustomId('starter')
-            .setPlaceholder('Quel est ta filière ?')
+            .setPlaceholder('Quelle est ta filière ?')
             .addOptions(
                 new StringSelectMenuOptionBuilder()
                     .setLabel('Création & Design')
@@ -39,11 +43,10 @@ module.exports = {
                     .setDescription('Si tu veux devenir le prochain Frank Lloyd Wright')
                     .setValue('architecture'),
             );
-
-
+        // liste des menus 2 pour choisir la filière 
         const select_class = new StringSelectMenuBuilder()
             .setCustomId('Classynov')
-            .setPlaceholder('en quel année est tu ?')
+            .setPlaceholder('En quelle année es-tu ?')
             .addOptions(
                 new StringSelectMenuOptionBuilder()
                     .setLabel('B1')
@@ -64,33 +67,111 @@ module.exports = {
                 new StringSelectMenuOptionBuilder()
                     .setLabel('M2')
                     .setValue('m2'),
+            );
 
-            )
-
+        // met les composants dans une variable
         const row = new ActionRowBuilder()
             .addComponents(select);
-
 
         const row2 = new ActionRowBuilder()
             .addComponents(select_class);
 
-
         const buttonEmailConfirm = new ButtonBuilder()
-            .setCustomId('confirm')
+            .setCustomId('confirmEmail')
             .setLabel('Cliquez ici pour entrer votre adresse email')
             .setStyle(ButtonStyle.Primary);
 
         const row3 = new ActionRowBuilder()
             .addComponents(buttonEmailConfirm);
+        // permet de dire que l'interaction comprend 3 choses
 
-        await interaction.reply({
+
+
+
+        // réponse à l'utlisateur avec le message incluant le menu déroulant
+        const response = await interaction.reply({
             content: 'Rentre tes infos :) !',
-            components: [row, row2, row3],
+            components: [row, row2, row3]
         });
-    },
-};
+        // attend la réponse de l'utilisateur
+
+
+        // Création d'un collecteur pour les interactions sur le menu déroulant
+        const selectMenuCollector1 = interaction.channel.createMessageComponentCollector({
+            componentType: ComponentType.StringSelect,  // Le type de composant (menu déroulant)
+            time: 3_600_000  // Temps pendant lequel le collecteur restera actif (1 heure dans ce cas)
+        });
+        
+        const selectMenuCollector2 = interaction.channel.createMessageComponentCollector({
+            componentType: ComponentType.StringSelect,  // Le type de composant (menu déroulant)
+            time: 3_600_000  // Temps pendant lequel le collecteur restera actif (1 heure dans ce cas)
+        });
+        
+        const buttonCollector = interaction.channel.createMessageComponentCollector({
+            componentType: ComponentType.Button,  // Le type de composant (bouton)
+            time: 3_600_000  // Temps pendant lequel le collecteur restera actif (1 heure dans ce cas)
+        });
+        
+        // Attendez les interactions de chaque collecteur
+        selectMenuCollector1.on('collect', async i => {
+           console.log(i.values[0]);
+        });
+        
+        selectMenuCollector2.on('collect', async i => {
+            // Code pour gérer l'interaction du deuxième menu déroulant
+            // Vous pouvez récupérer la sélection et répondre en conséquence ici
+            console.log(i.values[0]);
+        });
+        
+        buttonCollector.on('collect', async i => {
+          
+            console.log("le button à été cliqué ");
+        });
+
+
+    }
 
 
 
+
+
+}
+
+// gère l'interaction de manière asynchrone
+
+// collector.on('collect', async i => {
+//     if (i.interaction.isStringSelectMenu()) {
+
+//     }
+
+//     else if (interaction.isButton) {
+
+//         // Gérer l'interaction du bouton
+
+//             const modal = new ModalBuilder()
+//                 .setCustomId('myModal')
+//                 .setTitle('My Modal');
+
+//             const favoriteColorInput = new TextInputBuilder()
+//                 .setCustomId('favoriteColorInput')
+//                 .setLabel("What's your favorite color?")
+//                 .setStyle(TextInputStyle.Short);
+
+//             const hobbiesInput = new TextInputBuilder()
+//                 .setCustomId('hobbiesInput')
+//                 .setLabel("What's some of your favorite hobbies?")
+//                 .setStyle(TextInputStyle.Paragraph);
+
+//             const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
+//             const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
+
+//             modal.addComponents(firstActionRow, secondActionRow);
+//             await interaction.showModal(modal);
+//             await i.reply(interaction.showModal(modal));
+//     }
+// });
+
+//     }
+// }
 
 
