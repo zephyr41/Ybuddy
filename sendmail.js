0698c3f98bc14df354e41736bb4657bd42bb2dc1
Zephyr <nils.jaudon@ynov.com>
2023-09-20 22:16:54 +0200

    SendEmail

--- sendmail.js
+++ sendmail.js
@@ -5,16 +5,16 @@ async function sendEmail(email, password) {
   let transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',  // Vérifiez le bon host pour le service Brevo
     port: 587,
-    secure: false, // false pour STARTTLS
+    secure: false, 
     auth: {
-      user: '79zkpsxht5@privaterelay.appleid.com',  // Mettez votre identifiant SMTP correct ici
-      pass: '7P0kyKb1SWcgxhj8',  // Mettez votre mot de passe SMTP correct ici
+      user: '79zkpsxht5@privaterelay.appleid.com',  
+      pass: '7P0kyKb1SWcgxhj8',  
     },
   });
 
   // Paramètres de l'e-mail
   let mailOptions = {
-    from: 'YBUDDY <ybuddyEmail@example.com>', // Mettez votre adresse e-mail ici
+    from: 'YBUDDY <ybuddyEmail@example.com>', 
     to: email,
     subject: 'Votre mot de passe',
     text: `Votre mot de passe est "" `,
@@ -28,7 +28,7 @@ async function sendEmail(email, password) {
 
 // Utilisation de la fonction pour envoyer l'e-mail
 const email = "nils.jaudon@orange.fr";
-const password = '7P0kyKb1SWcgxhj8'; // Remplacez par le mot de passe
+const password = '7P0kyKb1SWcgxhj8'; 
 sendEmail(email, password)
   .then(() => console.log('Email envoyé avec succès'))
   .catch((error) => console.error('Erreur lors de l\'envoi de l\'email :', error));
